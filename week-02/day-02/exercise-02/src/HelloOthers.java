ee1a5bf391363fd1163e27ab7d6db9918c8c5f4d
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-03-21 16:17:48 +0100

    (feat) the program greets others with sout

---
+++ week-02/day-02/exercise-02/src/HelloOthers.java
@@ -0,0 +1,11 @@
+/**
+ * Created by dr. Brandstädter Tamás on 2017.03.21..
+ */
+public class HelloOthers {
+  public static void main(String[] args) {
+    System.out.println("Hello, Viktor!");
+    System.out.println("Hello, Podi!");
+    System.out.println("Hello, Zsuzsi!");
+  }
+}
+

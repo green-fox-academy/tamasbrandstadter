675443879b3929ab5f71176fab987f3727781628
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-03-21 16:19:31 +0100

    (feat) basic personal infos are displayed and sout-ed

---
+++ week-02/day-02/exercise-04/src/IntroduceYourself.java
@@ -0,0 +1,14 @@
+/**
+ * Created by dr. Brandstädter Tamás on 2017.03.21..
+ */
+public class IntroduceYourself {
+  public static void main(String[] args) {
+    String name = "Tamás Brandstädter dr.";
+    int age = 29;
+    double height = 1.75;
+
+    System.out.println("My name: " + name);
+    System.out.println("My age: " + age);
+    System.out.println("My height is: " + height + " meters");
+  }
+}

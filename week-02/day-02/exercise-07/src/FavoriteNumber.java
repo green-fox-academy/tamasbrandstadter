8686bf4ed6892f0d8ced813d8a28b3bf402e54e4
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-03-21 16:25:08 +0100

    FavouriteNumber(feat): my favourite number is sout-ed

---
+++ week-02/day-02/exercise-07/src/FavoriteNumber.java
@@ -0,0 +1,9 @@
+/**
+ * Created by dr. Brandstädter Tamás on 2017.03.21..
+ */
+public class FavoriteNumber {
+  public static void main(String[] args) {
+    int favorite_number = 8;
+    System.out.println("My favourite number is: " + favorite_number);
+  }
+}

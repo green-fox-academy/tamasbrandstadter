16007e720060b770083d440eeaacefd5eceff2e1
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-03-21 16:22:32 +0100

    Calculation(feat): various calculations with two ints

---
+++ week-02/day-02/exercise-05/src/TwoNumbers.java
@@ -0,0 +1,17 @@
+/**
+ * Created by dr. Brandstädter Tamás on 2017.03.21..
+ */
+public class TwoNumbers {
+  public static void main(String[] args) {
+    int a = 22;
+    int b = 13;
+
+    System.out.println(b + a);
+    System.out.println(b - a);
+    System.out.println(a * b);
+    System.out.println(a / b);
+    System.out.println((float) a / b);
+    System.out.println(a % b);
+  }
+}
+

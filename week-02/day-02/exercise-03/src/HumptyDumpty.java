08cecc555f3422a626169b61066cc17ac3058395
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-03-21 16:18:25 +0100

    (feat) the lines of the poet is in order

---
+++ week-02/day-02/exercise-03/src/HumptyDumpty.java
@@ -0,0 +1,12 @@
+/**
+ * Created by dr. Brandstädter Tamás on 2017.03.21..
+ */
+public class HumptyDumpty {
+  public static void main(String[] args) {
+    System.out.println("Humpty Dumpty sat on a wall,");
+    System.out.println("Humpty Dumpty had a great fall.");
+    System.out.println("Four-score Men and Four-score more,");
+    System.out.println("Could not make Humpty Dumpty where he was before.");
+  }
+}
+

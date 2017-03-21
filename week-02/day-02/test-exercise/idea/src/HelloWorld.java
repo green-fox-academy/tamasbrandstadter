29a19c0b27ea16d6e2a795cf9ba06dc066fdedb4
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-03-21 16:12:47 +0100

    version(final) working program feature

---
+++ week-02/day-02/test-exercise/idea/src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by dr. Brandstädter Tamás on 2017.03.21..
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("alma");
+    }
+}
+

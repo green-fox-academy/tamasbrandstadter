492f027b1adf5ee4515fe1993f59624ca09d10f4
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-03-21 17:43:07 +0100

    AnimalsAndLegs(feat): number of animals input by user from which number of legs are calculated

---
+++ week-02/day-02/exercise-16/src/AnimalsAndLegs.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AnimalsAndLegs {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int userInput1 = scanner.nextInt();
+    int userInput2 = scanner.nextInt();
+
+    System.out.println((userInput1*2) + (userInput2*4) + " legs in total.");
+
+
+  }
+}

68712c750dc844d40b0918d4cf471718c5e5f24d
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-03-21 16:25:58 +0100

    DefineInfos(feat): detailed infos are displayed

---
+++ week-02/day-02/exercise-08/src/DefineBasicInfo.java
@@ -0,0 +1,15 @@
+/**
+ * Created by dr. Brandstädter Tamás on 2017.03.21..
+ */
+public class DefineBasicInfo {
+  public static void main(String[] args) {
+    String myName = "dr. Brandstädter Tamás";
+    int myAge = 29;
+    double myHeight = 1.75;
+    boolean married = true;
+    System.out.println("Name: " + myName);
+    System.out.println("Age: " + myAge);
+    System.out.println("Height: " + myHeight);
+    System.out.println("Married: " + married);
+  }
+}

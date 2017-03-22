073f89cccda8be0a0a8407541fa6623a9f656a83
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-03-22 14:55:57 +0100

    (feat) the function doubles the input parameter and returns it

---
+++ week-02/day-03/exercise-01/src/Doubling.java
@@ -0,0 +1,13 @@
+public class Doubling {
+  public static void main(String[] args) {
+    int ak = 123;
+    System.out.println(doubling(ak));
+  }
+
+
+  public static int doubling(int ak) {
+    return ak*2;
+  }
+}
+
+

9613ba1970b6d6a0c4c3017cf56186d2526044a8
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-03-22 12:11:38 +0100

    Cheat(feat): String printed out 100 times with for loop

---
+++ week-02/day-02/exercise-23/src/IWontCheat.java
@@ -0,0 +1,11 @@
+public class IWontCheat {
+  public static void main(String[] args) {
+
+    String quote = "I won't cheat on the exam!";
+
+    for (int i = 0; i < 100; i++) {
+      System.out.println(quote);
+    }
+
+  }
+}

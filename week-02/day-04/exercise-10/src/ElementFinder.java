a5c7a2666a110867aa9b18a47e1086a0b4bdd1ee
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-03-29 21:09:40 +0200

    condition examines if arraylist contains a number

---
+++ week-02/day-04/exercise-10/src/ElementFinder.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ElementFinder {
+  public static void main(String[] args) {
+    ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
+
+    if (arrayList.contains(7)) {
+      System.out.println("Horray");
+    }
+    else {
+      System.out.println("noooo");
+    }
+
+    int index = arrayList.indexOf(7);
+    if (index == -1) {
+      System.out.println("Nooo");
+    }
+    else {
+      System.out.println("Horray.");
+    }
+  }
+}

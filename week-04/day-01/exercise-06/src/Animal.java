608068fbf565a821d37b80f2f5c16f312946dac2
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-04-03 14:33:05 +0200

    programs counts Cat object hunger and thirst by its behaviour

---
+++ week-04/day-01/exercise-06/src/Animal.java
@@ -0,0 +1,36 @@
+public class Animal {
+  int hunger;
+  int thirst;
+
+  public Animal() {
+    this(50, 50);
+  }
+
+  public Animal (int hunger, int thirst) {
+    this.hunger = hunger;
+    this.thirst = thirst;
+  }
+
+  public int eat() {
+    return hunger--;
+  }
+
+  public int drink() {
+    return thirst--;
+  }
+
+  public void play(){
+    hunger++;
+    thirst++;
+  }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ week-04/day-01/exercise-06/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+    Animal cat = new Animal();
+
+    cat.eat();
+    System.out.println(cat.hunger);
+    cat.drink();
+    System.out.println(cat.thirst);
+    cat.play();
+    System.out.println(cat.thirst + " " + cat.hunger);
+
+
+
+
+
+
+  }
+}

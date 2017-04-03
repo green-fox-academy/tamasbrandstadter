be3d22d278ab222415dd96562d4626b88d43e052
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-04-03 11:30:18 +0200

    new objects created from class and by method in main

---
+++ week-04/day-01/exercise-02/src/BlogPost.java
@@ -0,0 +1,18 @@
+public class BlogPost{
+    String authorname;
+    String title;
+    String text;
+    String publicationDate;
+
+
+
+  public BlogPost (String a, String t, String tt, String pD) {
+   authorname = a;
+   t = title;
+   tt = text;
+   pD = publicationDate;
+
+  }
+}
+
+

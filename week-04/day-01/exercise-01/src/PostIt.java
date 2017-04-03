0a8fff9cc06171a6d36b53df5a5eda927452250c
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-04-03 11:15:45 +0200

    new objects created from classes

---
+++ week-04/day-01/exercise-01/src/PostIt.java
@@ -0,0 +1,19 @@
+public class PostIt {
+  public static void main(String[] args) {
+    PostItExample postIt1 = new PostItExample();
+    postIt1.backgroundColor = "orange";
+    postIt1.text = "Idea1";
+    postIt1.textColor = "blue";
+
+    PostItExample postIt2 = new PostItExample();
+    postIt2.backgroundColor = "pink";
+    postIt2.text = "Awesome";
+    postIt2.textColor = "black";
+
+
+    PostItExample postIt3 = new PostItExample();
+    postIt3.backgroundColor = "yellow";
+    postIt3.text = "Superb";
+    postIt3.textColor = "green";
+  }
+}
---
+++ week-04/day-01/exercise-01/src/PostItExample.java
@@ -0,0 +1,7 @@
+public class PostItExample {
+  String backgroundColor;
+  String text;
+  String textColor;
+  PostIt posIt1 = new PostIt();
+
+}

0b79fa96e3cdbdb3abe748c46bf23f66389632f0
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-04-05 11:16:42 +0200

    added summarizing method and test

--- week-04/day-03/exercise-02/src/Sum.java
+++ week-04/day-03/exercise-02/src/Sum.java
@@ -7,4 +7,11 @@ public class Sum {
 
   }
 
+  public int sumIntegers(ArrayList<Integer> myIntArrayList) {
+    int sum = 0;
+    for (int i = 0; i < myIntArrayList.size() ; i++) {
+      sum += myIntArrayList.get(i);
+    }
+    return sum;
+  }
 }
---
+++ week-04/day-03/exercise-02/test/SumTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SumTest {
+
+  @Test
+  public void sumIntegers() throws Exception {
+    
+
+  }
+
+}

c54abb7502997939fd1b0174436aeb0e41b38010
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-04-05 14:57:36 +0200

    modified method

--- week-04/day-03/exercise-04/src/CountLetters.java
+++ week-04/day-03/exercise-04/src/CountLetters.java
@@ -1,26 +1,22 @@
 import java.util.HashMap;
 
 public class CountLetters {
-  HashMap<Character, Integer> hm = new HashMap<>();
+  HashMap<String, Integer> hm = new HashMap<>();
 
   public CountLetters() {
     this.hm = hm;
   }
 
-  public HashMap<Character, Integer> countLetters(String text) {
+  public HashMap<String, Integer> countLetters(String text) {
 
     for (int i = 0; i < text.length(); i++) {
-      if (hm != null) {
-        hm.putIfAbsent(text.charAt(i), 1);
+      if (hm.containsKey((text.substring(i, i + 1)))) {
+        hm.put(text.substring(i, i + 1), hm.get(text.substring(i, i + 1) + 1));
       } else {
-        Integer count = hm.get(text.charAt(i));
-        hm.putIfAbsent(text.charAt(i), count + 1);
-
+        hm.put(text.substring(i, i + i), + 1);
       }
-
     }
     return hm;
   }
 }
-      
 
---
+++ week-04/day-03/exercise-04/test/CountLettersTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountLettersTest {
+  @Test
+  public void countLetters() throws Exception {
+    CountLetters countLetters = new CountLetters();
+    System.out.println(countLetters.countLetters("asdasdfghhgf"));
+
+  }
+
+}

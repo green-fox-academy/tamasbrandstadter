a218c961f8d16e58a989d5e50ef1ea5aa9787f9c
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-04-05 11:35:58 +0200

    test for empty ArrayList

--- week-04/day-03/exercise-02/test/SumTest.java
+++ week-04/day-03/exercise-02/test/SumTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -6,15 +7,26 @@ import static org.junit.Assert.*;
 
 public class SumTest {
 
+
   @Test
-  public void sumIntegers() throws Exception {
+  public void testSumIntegers() throws Exception {
     Sum sum = new Sum();
     ArrayList<Integer> myIntArrayList = new ArrayList<>();
 
     for (int i = 0; i < 5 ; i++) {
       myIntArrayList.add(i, i+1);
     }
-
     assertEquals(15, sum.sumIntegers(myIntArrayList));
   }
+
+  @Test
+  public void testEmptyList() throws Exception {
+    Sum sum = new Sum();
+    ArrayList<Integer> myIntArrayList = new ArrayList<>();
+    assertEquals(0, sum.sumIntegers(myIntArrayList));
+  }
+
+  
+
+
 }

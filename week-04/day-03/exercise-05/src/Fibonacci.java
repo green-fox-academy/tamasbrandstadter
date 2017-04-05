63ba84b89ef8239943685d8d0b01879fc55c5e5e
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-04-05 15:50:26 +0200

    Fibonacci sequence method created

---
+++ week-04/day-03/exercise-05/src/Fibonacci.java
@@ -0,0 +1,27 @@
+public class Fibonacci {
+  private int nNumber;
+
+  public Fibonacci() {
+    this.nNumber = nNumber;
+
+  }
+
+  public int fibonacciMethod(int nNumber) {
+
+    if (nNumber == 0) {
+      return 0;
+    } if (nNumber == 1) {
+      return 1;
+    } else {
+      nNumber = fibonacciMethod(nNumber - 2) + fibonacciMethod(nNumber - 1);
+      return nNumber;
+    }
+    
+
+
+  }
+
+}
+
+
+

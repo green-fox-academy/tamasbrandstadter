831e3bac61a0e44b92979538720da8be25f0cabb
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-04-09 15:06:10 +0200

    (core)feat: array elements squared

---
+++ week-02/day-03/exercise-14/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    int[] ag = {3, 4, 5, 6, 7};
+
+    for (int i = 0; i < ag.length; i++) {
+      ag[i] *= 2;
+      System.out.println(ag[i]);
+    }
+  }
+}

ac0644b642f818142ee36ef0adccd51dceba712e
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-04-11 10:35:31 +0200

    PositionedImage class created

--- week-05/tk-wanderer/src/Board.java
+++ week-05/tk-wanderer/src/Board.java
@@ -1,2 +1,55 @@
-public class Board {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Board extends JComponent implements KeyListener {
+
+  int testBoxX;
+  int testBoxY;
+
+  public Board() {
+    testBoxX = 300;
+    testBoxY = 300;
+
+    // set the size of your draw board
+    setPreferredSize(new Dimension(720, 720));
+    setVisible(true);
+  }
+
+  @Override
+  public void paint(Graphics graphics) {
+    super.paint(graphics);
+    graphics.fillRect(testBoxX, testBoxY, 100, 100);
+    // here you have a 720x720 canvas
+    // you can create and draw an image using the class below e.g.
+    PositionedImage image = new PositionedImage("yourimage.png", 300, 300);
+    image.draw(graphics);
+  }
+
+
+  // To be a KeyListener the class needs to have these 3 methods in it
+  @Override
+  public void keyTyped(KeyEvent e) {
+
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+
+  }
+
+  // But actually we can use just this one for our goals here
+  @Override
+  public void keyReleased(KeyEvent e) {
+    // When the up or down keys hit, we change the position of our box
+    if (e.getKeyCode() == KeyEvent.VK_UP) {
+      testBoxY -= 100;
+    } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+      testBoxY += 100;
+    }
+    // and redraw to have a new picture with the new coordinates
+    repaint();
+  }
 }
+

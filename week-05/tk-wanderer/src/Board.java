3ce50e409e40017c8401c5e84cb52a63a5608800
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-04-11 11:53:52 +0200

    hero moves to left and right

--- week-05/tk-wanderer/src/Board.java
+++ week-05/tk-wanderer/src/Board.java
@@ -44,10 +44,7 @@ public class Board extends JComponent implements KeyListener {
   public void paint(Graphics graphics) {
     super.paint(graphics);
 
-    // here you have a 720x720 canvas
-    // you can create and draw an image using the class below e.g.
-
-    for (int i = 0; i < 10; i++) {
+     for (int i = 0; i < 10; i++) {
       for (int j = 0; j < 10; j++) {
         if (arrayForMap[i][j] == 1) {
           PositionedImage wallImage = new PositionedImage("assets/wall.png", j * 72, i * 72);
@@ -73,16 +70,19 @@ public class Board extends JComponent implements KeyListener {
 
   }
 
-  // But actually we can use just this one for our goals here
+
   @Override
   public void keyReleased(KeyEvent e) {
-    // When the up or down keys hit, we change the position of our box
+
     if (e.getKeyCode() == KeyEvent.VK_UP) {
       testBoxY -= 72;
     } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
       testBoxY += 72;
+    } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+      testBoxX += 72;
+    } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+      testBoxX -= 72;
     }
-    // and redraw to have a new picture with the new coordinates
     repaint();
   }
 }

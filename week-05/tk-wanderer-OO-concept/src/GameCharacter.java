fec19c23c30a6639042e3ec7e2adb06ed16d991a
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-04-12 17:16:17 +0200

    fixed if statement in move methods

--- week-05/tk-wanderer-OO-concept/src/GameCharacter.java
+++ week-05/tk-wanderer-OO-concept/src/GameCharacter.java
@@ -5,25 +5,25 @@ public class GameCharacter extends GameObject {
   }
 
   public void moveUp() {
-    if ((getPosY() - 72) >= 0 && (GameMap.floorMap[getPosY()/72 - 1][getPosY()/72] != 0)) {
+    if ((getPosY() - 72) >= 0 && (GameMap.floorMap[getPosY()/72 - 1][getPosX()/72] != 1)) {
       move(0, -72);
     }
   }
 
   public void moveDown() {
-    if ((getPosY() - 72) < 720 && (GameMap.floorMap[getPosY()/72 + 1][getPosY()/72] != 0)) {
+    if ((getPosY() - 72) < 720 && (GameMap.floorMap[getPosY()/72 + 1][getPosX()/72] != 1)) {
       move(0, 72);
     }
   }
 
   public void moveLeft(){
-    if ((getPosX() - 72) >= 0 && (GameMap.floorMap[getPosY()/72][getPosY()/72 +1] != 0)) {
+    if ((getPosX() - 72) >= 0 && (GameMap.floorMap[getPosY()/72][getPosX()/72 - 1] != 1)) {
       move(-72, 0);
     }
   }
 
   public void moveRight(){
-    if ((getPosX() - 72) < 720 && (GameMap.floorMap[getPosY()/72][getPosX()/72 - 1] != 0)) {
+    if ((getPosX() - 72) < 720 && (GameMap.floorMap[getPosY()/72][getPosX()/72 + 1] != 1)) {
       move(+72, 0);
     }
   }

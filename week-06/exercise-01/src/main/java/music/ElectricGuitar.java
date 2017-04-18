b1dffb4297428bcfc2da5bfa2c120fbfb05ee208
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-04-18 16:47:02 +0200

    created package + abstract classes

---
+++ week-06/exercise-01/src/main/java/music/ElectricGuitar.java
@@ -0,0 +1,12 @@
+package main.java.music;
+
+/**
+ * Created by Kékesi László on 2017.04.18..
+ */
+public class ElectricGuitar extends StringedInstrument {
+
+  @Override
+  void play() {
+
+  }
+}
---
+++ week-06/exercise-01/src/main/java/music/StringedInstrument.java
@@ -0,0 +1,7 @@
+package main.java.music;
+
+public abstract class StringedInstrument extends Instrument {
+  private int numberOfStrings;
+  private final String formatForPlay ="%s, a %d-stringed instrument that %s\n";
+
+}

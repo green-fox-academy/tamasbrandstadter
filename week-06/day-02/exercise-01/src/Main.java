dc803eca71055307ead7930c6d5f90021c2f63cc
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-04-19 13:41:18 +0200

    finished loop for counting types and colors

--- week-06/day-02/exercise-01/src/Main.java
+++ week-06/day-02/exercise-01/src/Main.java
@@ -9,18 +9,24 @@ public class Main {
     }
 
     int sumPorsche = 0;
-    int sumFerrai = 0;
+    int sumFerrari = 0;
     int sumAudi = 0;
     int sumBmw = 0;
     int sumLamborghini = 0;
     int sumMercedes = 0;
+    int sumBlack = 0;
+    int sumWhite = 0;
+    int sumYellow = 0;
+    int sumBlue = 0;
+    int sumGreen = 0;
+    int sumRed = 0;
 
     for (int i = 0; i < carList.size(); i++) {
       if (carList.get(i).getType().equals(Types.PORSCHE)) {
         sumPorsche++;
       }
       if (carList.get(i).getType().equals(Types.FERRARI)) {
-        sumFerrai++;
+        sumFerrari++;
       }
       if (carList.get(i).getType().equals(Types.AUDI)) {
         sumAudi++;
@@ -34,8 +40,28 @@ public class Main {
       if (carList.get(i).getType().equals(Types.BMW)) {
         sumBmw++;
       }
+      if (carList.get(i).getColor().equals(Color.BLACK)) {
+        sumBlack++;
+      }
+      if (carList.get(i).getColor().equals(Color.WHITE)) {
+        sumWhite++;
+      }
+      if (carList.get(i).getColor().equals(Color.RED)) {
+        sumRed++;
+      }
+      if (carList.get(i).getColor().equals(Color.YELLOW)) {
+        sumYellow++;
+      }
+      if (carList.get(i).getColor().equals(Color.BLUE)) {
+        sumBlue++;
+      }
+      if (carList.get(i).getColor().equals(Color.GREEN)) {
+        sumGreen++;
+      }
     }
-    System.out.println("Sum of Porshe: " + sumPorsche + " Sum of Audi: " + sumAudi + " Sum of Ferrari: " + sumFerrai +
+    System.out.println("Sum of Porshe: " + sumPorsche + " Sum of Audi: " + sumAudi + " Sum of Ferrari: " + sumFerrari +
             " Sum of BMW: " + sumBmw + " Sum of Lamborghini: " + sumLamborghini + " Sum of Mercedes: " + sumMercedes);
+    System.out.println("Sum of yellow: " + sumYellow + " Sum of blue: " + sumBlue + " Sum of white: " + sumWhite +
+    " Sum of black: " + sumBlack + " Sum of red: " + sumRed + " Sum of green: " + sumGreen);
   }
 }

4c5f5844f24e6856d2f532727a940d484bb252e8
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-02 16:51:45 +0200

    refactored method for thymeleaf solution

---
+++ week-07/day-02/exercise-01/src/main/java/com/greenfoxacademy/springstart/Hello.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.springstart;
+
+public class Hello {
+  String greet;
+
+  public Hello(String greet) {
+    this.greet = greet;
+  }
+
+  public String getGreet() {
+    return greet;
+  }
+}
--- week-07/day-02/exercise-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-02/exercise-01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,44 +1,33 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import com.greenfoxacademy.springstart.Hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
   private AtomicLong atomicLong = new AtomicLong();
+  private final String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
 
   @RequestMapping("/web/greeting")
   public String greeting(Model model, @RequestParam(value = "name") String name) {
+    List<Hello> helloList = new ArrayList<>();
+    for (String greet : hellos) {
+         helloList.add(new Hello(greet));
+    }
     model.addAttribute("name", name);
     model.addAttribute("count", atomicLong.addAndGet(1));
-    model.addAttribute("hellos", displayHello());
+    model.addAttribute("hellos", helloList);
     return "greeting";
-  }
-
-  public String displayHello() {
-    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
-            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
-            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
-            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
-    String str = "";
-    for (int i = 0; i < hellos.length; i++) {
-      str += hellos[i] + " ";
-    }
-    return str;
-  }
-
-  public String getRandomColor() {
-    final Random random = new Random();
-    final String[] letters = "0123456789ABCDEF".split("");
-    String color = "#";
-    for (int i = 0; i < 6; i++) {
-      color += letters[Math.round(random.nextFloat() * 15)];
-    }
-    return color;
   }
 }

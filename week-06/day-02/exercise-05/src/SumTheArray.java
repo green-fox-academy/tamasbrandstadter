4de05072b9341efcdff29c0763fb8b231c8e88ed
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-02 21:36:50 +0200

    fixed indentation

--- week-06/day-02/exercise-05/src/SumTheArray.java
+++ week-06/day-02/exercise-05/src/SumTheArray.java
@@ -1,24 +1,24 @@
 public class SumTheArray {
-    public static void main(String[] args) {
-        Integer[] inputArray = {1, 2, 3, 4, 5, 6};
-        System.out.println(sumTheArray(inputArray, 6));
-    }
+  public static void main(String[] args) {
+    Integer[] inputArray = {1, 2, 3, 4, 5, 6};
+    System.out.println(sumTheArray(inputArray, 6));
+  }
 
-    public static <T extends Number> int sumTheArray(T[] inputArray, int lenght) {
-        int[] array = new int[inputArray.length];
-        int sum = 0;
-        for (int i = 0; i < inputArray.length; i++) {
-            array[i] = inputArray[(inputArray.length - 1 - i)].intValue();
-        }
-        try {
-            for (int i = 0; i < lenght; i++) {
-                sum += array[i];
-            }
-            return sum;
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            ex.printStackTrace();
-            System.out.println("Array index out of bounds.");
-            return 0;
-        }
+  public static <T extends Number> int sumTheArray(T[] inputArray, int lenght) {
+    int[] array = new int[inputArray.length];
+    int sum = 0;
+    for (int i = 0; i < inputArray.length; i++) {
+      array[i] = inputArray[(inputArray.length - 1 - i)].intValue();
     }
+    try {
+      for (int i = 0; i < lenght; i++) {
+        sum += array[i];
+      }
+      return sum;
+    } catch (ArrayIndexOutOfBoundsException ex) {
+      ex.printStackTrace();
+      System.out.println("Array index out of bounds.");
+      return 0;
+    }
+  }
 }

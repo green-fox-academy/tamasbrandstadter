894df3d375d74438d7ffe4246fd05ab19b253ba2
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-02 11:51:12 +0200

    created method for displaying id and content on localhost

--- week-07/day-02/exercise-01/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
+++ week-07/day-02/exercise-01/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -1,10 +1,12 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Greeting {
   private long id;
   private String content;
-
-
-  
 
 }

68fe36a27af855b1243f8d4edc1f740b19e69dc1
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-02 19:46:02 +0200

    core(feat): reservation code randomly generated and displayed

---
+++ week-06/day-01/exercise-03/src/com/greenfox/bx/ReservationApp.java
@@ -0,0 +1,12 @@
+package com.greenfox.bx;
+
+public class ReservationApp {
+    public static void main(String[] args) {
+        Reservation reservation = new Reservation();
+        System.out.println(reservation.toString());
+        System.out.println(reservation.toString());
+        System.out.println(reservation.toString());
+        System.out.println(reservation.toString());
+        System.out.println(reservation.toString());
+    }
+}

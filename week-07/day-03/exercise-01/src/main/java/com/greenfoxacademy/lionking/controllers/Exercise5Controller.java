c51b15343ed37a3da9e40b8457bc5833047d2bdf
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-03 14:11:04 +0200

    names of arraylist objects are displayed by thymeleaf foreach

---
+++ week-07/day-03/exercise-01/src/main/java/com/greenfoxacademy/lionking/controllers/Exercise5Controller.java
@@ -0,0 +1,24 @@
+package com.greenfoxacademy.lionking.controllers;
+
+import com.greenfoxacademy.lionking.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class Exercise5Controller {
+  private List<BankAccount> bankAccountList = new ArrayList<>();
+
+  @RequestMapping("/objects")
+  public String exerciseDisplay(Model model) {
+    bankAccountList.add(new BankAccount("Pumba", "5000", "boar"));
+    bankAccountList.add(new BankAccount("Timon", "6000", "meerkat"));
+    bankAccountList.add(new BankAccount("Nala", "7000", "lion"));
+    bankAccountList.add(new BankAccount("Rafiki", "8000", "mandrill"));
+    model.addAttribute("objects", bankAccountList);
+    return "objects";
+  }
+}

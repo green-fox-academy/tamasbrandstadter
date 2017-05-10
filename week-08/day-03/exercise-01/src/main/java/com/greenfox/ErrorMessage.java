23429e53cee4065016b958592523b15f86bf6661
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-10 12:10:01 +0200

    created ErrorMessage class and method handles exception

---
+++ week-08/day-03/exercise-01/src/main/java/com/greenfox/ErrorMessage.java
@@ -0,0 +1,13 @@
+package com.greenfox;
+
+public class ErrorMessage {
+  private String message;
+
+  public ErrorMessage(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
--- week-08/day-03/exercise-01/src/main/java/com/greenfox/MainRestController.java
+++ week-08/day-03/exercise-01/src/main/java/com/greenfox/MainRestController.java
@@ -1,6 +1,7 @@
 package com.greenfox;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,10 @@ public class MainRestController {
     doubleNumber.setReceived(input);
     doubleNumber.setResult(input*2);
     return doubleNumber;
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ErrorMessage showError(Exception e) {
+    return new ErrorMessage("Please provide an input!");
   }
 }

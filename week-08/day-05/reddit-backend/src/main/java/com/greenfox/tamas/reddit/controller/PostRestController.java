07dae41f5cee5870a7f48d27799dac39b83fa2a2
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-12 12:00:15 +0200

    created first endpoint for listing posts

--- week-08/day-05/reddit-backend/src/main/java/com/greenfox/tamas/reddit/controller/PostRestController.java
+++ week-08/day-05/reddit-backend/src/main/java/com/greenfox/tamas/reddit/controller/PostRestController.java
@@ -1,8 +1,22 @@
 package com.greenfox.tamas.reddit.controller;
 
+import com.greenfox.tamas.reddit.model.Posts;
+import com.greenfox.tamas.reddit.services.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostRestController {
 
+  @Autowired
+  private PostRepository postRepository;
+
+  @RequestMapping(value = "/posts", method = RequestMethod.GET)
+  public Posts listPosts() {
+    Posts posts = new Posts();
+    posts.setPosts(postRepository.findAll());
+    return posts;
+  }
 }
--- week-08/day-05/reddit-backend/src/main/java/com/greenfox/tamas/reddit/model/Post.java
+++ week-08/day-05/reddit-backend/src/main/java/com/greenfox/tamas/reddit/model/Post.java
@@ -1,9 +1,62 @@
 package com.greenfox.tamas.reddit.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
 public class Post {
-  private Long id;
-  private String title;
-  private String href;
-  private int score;
-  private int timestamp;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  Long id;
+  String title;
+  String href;
+  int score;
+  Timestamp timestamp;
+
+  public Post() {
+
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getHref() {
+    return href;
+  }
+
+  public void setHref(String href) {
+    this.href = href;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
+
+  public Timestamp getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(Timestamp timestamp) {
+    this.timestamp = timestamp;
+  }
 }

8b292d64182df3bcf42428c3071e3c7a69c61499
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-12 10:47:11 +0200

    created new database and connected

---
+++ week-08/day-05/reddit-backend/src/main/java/com/greenfox/tamas/reddit/controller/PostRestController.java
@@ -0,0 +1,8 @@
+package com.greenfox.tamas.reddit.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostRestController {
+
+}
--- week-08/day-05/reddit-backend/src/main/java/com/greenfox/tamas/reddit/model/Post.java
+++ week-08/day-05/reddit-backend/src/main/java/com/greenfox/tamas/reddit/model/Post.java
@@ -4,4 +4,6 @@ public class Post {
   private Long id;
   private String title;
   private String href;
+  private int score;
+  private int timestamp;
 }

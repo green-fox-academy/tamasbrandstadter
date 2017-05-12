b709f9ce7bb31882c7a948df6333344fee413958
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-12 14:46:48 +0200

    created User class

---
+++ week-08/day-05/reddit-backend/src/main/java/com/greenfox/tamas/reddit/model/User.java
@@ -0,0 +1,17 @@
+package com.greenfox.tamas.reddit.model;
+
+public class User {
+  String userName;
+
+  public User() {
+
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+}

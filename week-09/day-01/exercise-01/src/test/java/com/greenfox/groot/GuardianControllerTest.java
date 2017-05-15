4e5a36d796f4cb69b2ce4bee90cc2c6833a5796b
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-15 12:06:19 +0200

    created tests and made them pass

--- week-09/day-01/exercise-01/src/test/java/com/greenfox/groot/GuardianControllerTest.java
+++ week-09/day-01/exercise-01/src/test/java/com/greenfox/groot/GuardianControllerTest.java
@@ -1,18 +1,20 @@
 package com.greenfox.groot;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import java.nio.charset.Charset;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
@@ -20,10 +22,6 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @WebAppConfiguration
 @EnableWebMvc
 public class GuardianControllerTest {
-
-  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-          MediaType.APPLICATION_JSON.getSubtype(),
-          Charset.forName("utf8"));
 
   private MockMvc mockMvc;
 
@@ -34,4 +32,19 @@ public class GuardianControllerTest {
   public void setup() throws Exception {
     this.mockMvc = webAppContextSetup(webApplicationContext).build();
   }
+
+  @Test
+  public void testWithParameter() throws Exception {
+    mockMvc.perform(get("/groot?message=somemessage"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.received").value("somemessage"))
+            .andExpect(status().isOk());
+  }
+
+  @Test
+  public void testWithOutParameter() throws Exception {
+    mockMvc.perform(get("/groot"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.error").value("I am groot!"))
+            .andExpect(status().isOk());
+  }
 }
+

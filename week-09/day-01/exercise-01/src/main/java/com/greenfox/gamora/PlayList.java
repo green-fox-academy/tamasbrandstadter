2d1b3c98f0097469f877e836486bf7d29d166265
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-05-16 14:44:49 +0200

    created endpoints for adding, deleting, updating

--- week-09/day-01/exercise-01/src/main/java/com/greenfox/gamora/PlayList.java
+++ week-09/day-01/exercise-01/src/main/java/com/greenfox/gamora/PlayList.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlayList {
-  List<Song> songList;
+  private List<Song> songList;
 
-  public PlayList(){
+  public PlayList() {
     this.songList = new ArrayList<>();
     songList.add(new Song("DJ Bobo", "It's my life", 1977, 10.0));
     songList.add(new Song("DJ Lalala", "Blabla", 1990, 5.5));
@@ -18,5 +18,30 @@ public class PlayList {
 
   public void setSongList(List<Song> songList) {
     this.songList = songList;
+  }
+
+  public void addSong(Song song) {
+    songList.add(song);
+  }
+
+  public void deleteSong(String title) {
+    for (Song song : songList) {
+      if (song.getTitle().equals(title)) {
+        songList.remove(song);
+      }
+    }
+  }
+
+  public void setRating(Song song, double rating) {
+    song.setRating(rating);
+  }
+
+  public Song findSong(String title) {
+    for (Song song : songList) {
+      if (song.getTitle().equals(title)) {
+        return song;
+      }
+    }
+    return null;
   }
 }
--- week-09/day-01/exercise-01/src/main/java/com/greenfox/gamora/SongRestController.java
+++ week-09/day-01/exercise-01/src/main/java/com/greenfox/gamora/SongRestController.java
@@ -1,9 +1,9 @@
 package com.greenfox.gamora;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/awesome")
 public class SongRestController {
   private PlayList playList;
 
@@ -11,8 +11,26 @@ public class SongRestController {
     this.playList = new PlayList();
   }
 
-  @GetMapping("/awesome")
+  @GetMapping("/")
   public PlayList listPlayList(){
+    return playList;
+  }
+
+  @PostMapping("/add")
+  public Song add(@RequestBody Song song) {
+    playList.addSong(song);
+    return song;
+  }
+
+  @DeleteMapping("/delete/{title}")
+  public PlayList delete(@PathVariable("title") String title) {
+    playList.deleteSong(title);
+    return playList;
+  }
+
+  @PutMapping("/change/{title}")
+  public PlayList updateRating(@PathVariable("title") String title, @RequestParam("rating") double rating) {
+    playList.setRating(playList.findSong(title), rating);
     return playList;
   }
 }

8c7d690037462feaf2041fbb401e5ed08e02fc85
tamasbrandstadter <tamas.brandstadter@gmail.com>
2017-07-10 10:06:14 +0200

    added more tests

--- warm-up-for-exam/test/MainTest.java
+++ warm-up-for-exam/test/MainTest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MainTest {
   private static List<String> testList;
@@ -26,5 +27,24 @@ class MainTest {
   @Test
   void testForReturnSecondMethod() {
     assertEquals(testList, Main.splitStringOther("a,bcd,e,fg", ','));
+  }
+
+  @Test
+  void testForElementOfList() {
+    List<String> returnedList = Main.splitString("a,bcd,e,fg", ',');
+    String s = testList.get(1);
+    assertEquals(s, returnedList.get(1));
+  }
+
+  @Test
+  void testForSize(){
+    List<String> returnedList = Main.splitString("a,bcd,e,fg", ',');
+    assertTrue(returnedList.size() == 4);
+    assertTrue(returnedList.size() == testList.size());
+  }
+  
+  @Test
+  void testForSizeOther(){
+    assertTrue(4 == Main.splitString("a,bcd,e,fg", ',').size());
   }
 }
